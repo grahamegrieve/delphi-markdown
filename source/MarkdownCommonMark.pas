ffc79818cfcab03a4eb6921803a9f5152657d23b
Grahame Grieve <grahameg@gmail.com>
2019-01-04 00:19:01 +1100

    more common mark work

--- source/MarkdownCommonMark.pas
+++ source/MarkdownCommonMark.pas
@@ -1,15 +1,24 @@
 Unit MarkdownCommonMark;
 
+{
+currently working:
+- headings
+- code blocks (+fenced)
+- thematic blocks
+
+not supported
+- HTML blocks
+}
 interface
 
 uses
   SysUtils, Generics.Collections;
 
 
+
 // Abstract Syntax Tree
 type
-  TMarkdownObject = class abstract (TObject);
-  TBlock = class abstract (TMarkdownObject)
+  TBlock = class abstract (TObject)
   protected
     procedure render(b : TStringBuilder); virtual; abstract;
   end;
@@ -25,7 +34,7 @@ type
 
     property blocks : TObjectList<TBLock> read FBlocks;
   end;
-//  TQuoteBlock = class (TContainerBlock);
+
   TMarkdownDocument = class (TContainerBlock);
 //  TListItemBlock = class (TContainerBlock);
 //  TListBlock = class (TContainerBlock);
@@ -42,6 +51,11 @@ type
     property header : integer read FHeader write FHeader;
   end;
 
+  TQuoteBlock = class (TParagraphBlock)
+  protected
+    procedure render(b : TStringBuilder); override;
+  end;
+
   THeadingBlock = class (TContainerBlock)
   private
     FLevel: integer;
@@ -53,8 +67,14 @@ type
   end;
 
   TCodeBlock = class (TContainerBlock)
+  private
+    FFenced: boolean;
+    FLang: String;
   protected
     procedure render(b : TStringBuilder); override;
+  public
+    property fenced : boolean read FFenced write FFenced;
+    property lang : String read FLang write FLang;
   end;
 
   TLeafBlock = class abstract (TBlock);
@@ -72,15 +92,49 @@ type
   protected
     procedure render(b : TStringBuilder); override;
   end;
-
- //  TLinkReferenceDefinition = class (TLeafBlock);
+//  TLinkReferenceDefinition = class (TLeafBlock);
 //  THtmlBlock = class (TLeafBlock);
 //  TFencedCodeBlock = class (TCodeBLock);
-
 //  TBlankLineBlock = class (TBlock);
 
+  // Parser Infrastructure
+  TCommonMarkParser = class;
 
 // Parser
+  TBlockProcessor = class abstract (TObject)
+  protected
+    FParent : TBlockProcessor;
+
+    // this procedure processes a line for nested blocks
+    //
+    // return false if the block is done.
+    //
+    // if true, modify the line, removing prepended characters, and remember to grab the characters as you go
+    function processLine(var line : string; root : boolean) : boolean; virtual; abstract;
+    function parser : TCommonMarkParser; virtual;
+  public
+    constructor Create(processor : TBlockProcessor);
+  end;
+
+  // this anchors the chain
+  TDocumentProcessor = class (TBlockProcessor)
+  private
+    FParser : TCommonMarkParser;
+  protected
+    function processLine(var line : string; root : boolean) : boolean; override;
+    function parser : TCommonMarkParser; override;
+  public
+    constructor Create(parser : TCommonMarkParser);
+  end;
+
+  TQuoteProcessor = class (TBlockProcessor)
+  private
+    quote : TQuoteBlock;
+  protected
+    function processLine(var line : string; root : boolean) : boolean; override;
+  public
+    constructor Create(processor : TBlockProcessor; q : TQuoteBlock);
+  end;
 
   TCommonMarkParser = class (TObject)
   private
@@ -112,15 +166,19 @@ type
     function stripWhitespace(s : String) : String;
     function escapeHtml(s : String) : String;
 
-    function inPara(blocks : TObjectList<TBlock>) : boolean;
+    function inPara(blocks : TObjectList<TBlock>; canBeQuote : boolean) : boolean;
+    function inQuote(blocks : TObjectList<TBlock>; out q : TQuoteBlock) : boolean;
+    function isBlock(line : String) : boolean;
 
     function parseThematicBreak(blocks : TObjectList<TBlock>; line : String) : boolean;
     function parseHeader(blocks : TObjectList<TBlock>; line : String) : boolean;
     function parseCodeBlock(blocks : TObjectList<TBlock>; line : String) : boolean;
+    function parseFencedCodeBlock(blocks : TObjectList<TBlock>; line : String) : boolean;
     function parseSeTextHeader(blocks : TObjectList<TBlock>; line : String) : boolean;
+    function parseQuoteBlock(blocks : TObjectList<TBlock>; line : String; processor : TBlockProcessor) : boolean;
     procedure parseInline(blocks : TObjectList<TBlock>; line : String);
     procedure check;
-    procedure parse(block : TContainerBlock); overload;
+    procedure parse(block : TContainerBlock; processor : TBlockProcessor); overload;
   public
     Constructor Create;
     Destructor Destroy; override;
@@ -157,11 +215,71 @@ begin
   result := copy(s, 1, i);
 end;
 
+function copyTo(s : String; chs : TSysCharSet) : String;
+var
+  i : integer;
+begin
+  i := 1;
+  while (i <= s.Length) and not CharInSet(s[i], chs) do
+    inc(i);
+  if i > s.Length then
+    result := s
+  else
+    result := copy(s, 1, i-1);
+end;
+
+function copyWhile(s : String; chs : TSysCharSet) : String;
+var
+  i : integer;
+begin
+  i := 1;
+  while (i <= s.Length) and CharInSet(s[i], chs) do
+    inc(i);
+  if i > s.Length then
+    result := s
+  else
+    result := copy(s, 1, i-1);
+end;
+
+// allows up to 3 spaces before c; returns the length to remove from the front as well as a boolean for whether to match
+function startsWithWS(s : String; c : char; out length : integer) : boolean; overload;
+var
+  i : integer;
+begin
+  if s = '' then
+    exit(false);
+
+  length := 1;
+  for i := 1 to 3 do
+  begin
+    if (s.Length >= length) and (s[length] = ' ') then
+      inc(length);
+  end;
+  result := s[length] = c;
+end;
+
+function startsWithWS(s : String; c : String; out length : integer) : boolean; overload;
+var
+  i : integer;
+begin
+  if s = '' then
+    exit(false);
+
+  length := 1;
+  for i := 1 to 3 do
+  begin
+    if (s.Length >= length) and (s[length] = ' ') then
+      inc(length);
+  end;
+  result := copy(s, length, c.Length) = c;
+end;
+
 { TCommonMarkParser }
 
 class function TCommonMarkParser.parse(src: String): TMarkdownDocument;
 var
   this : TCommonMarkParser;
+  doc : TDocumentProcessor;
 begin
   this := TCommonMarkParser.Create;
   try
@@ -170,8 +288,13 @@ begin
     this.FLine := 1;
     this.FCol := 1;
     this.check;
-    result := TMarkdownDocument.Create;
-    this.parse(result);
+    doc := TDocumentProcessor.Create(this);
+    try
+      result := TMarkdownDocument.Create;
+      this.parse(result, doc);
+    finally
+      doc.Free;
+    end;
   finally
     this.free;
   end;
@@ -299,9 +422,41 @@ begin
   result := CharInSet(ch, [#10, #9, ' ']);
 end;
 
-function TCommonMarkParser.inPara(blocks: TObjectList<TBlock>): boolean;
+function TCommonMarkParser.inPara(blocks: TObjectList<TBlock>; canBeQuote : boolean): boolean;
 begin
   result := (blocks.Count > 0) and (blocks.Last is TParagraphBlock) and not (blocks.Last as TParagraphBlock).closed and ((blocks.Last as TParagraphBlock).header = 0);
+  if result and not canBeQuote and (blocks.Last is TQuoteBlock) then
+    result := false;
+end;
+
+function TCommonMarkParser.inQuote(blocks: TObjectList<TBlock>; out q: TQuoteBlock): boolean;
+begin
+  result := (blocks.Count > 0) and (blocks.Last is TQuoteBlock) and not (blocks.Last as TQuoteBlock).closed;
+  if result then
+    q := blocks.Last as TQuoteBlock;
+end;
+
+function TCommonMarkParser.isBlock(line: String): boolean;
+var
+  l : integer;
+begin
+  if startsWithWS(line, '***', l) then
+    exit(true);
+  if startsWithWS(line, '---', l) then
+    exit(true);
+  if startsWithWS(line, '___', l) then
+    exit(true);
+  if startsWithWS(line, '#', l) then
+    exit(true);
+  if startsWithWS(line, '`', l) then
+    exit(true);
+  if startsWithWS(line, '~', l) then
+    exit(true);
+  if startsWithWS(line, '>', l) then
+    exit(true);
+  if line.startsWith('    ') then // code block
+    exit(true);
+  result := false;
 end;
 
 function TCommonMarkParser.isWhitespace(s: String): boolean;
@@ -423,6 +578,29 @@ begin
   blocks.Add(TTextBlock.create(escapeHtml(line.Trim)));
 end;
 
+function TCommonMarkParser.parseQuoteBlock(blocks: TObjectList<TBlock>; line: String; processor : TBlockProcessor): boolean;
+var
+  s : String;
+  q : TQuoteBlock;
+  qp : TQuoteProcessor;
+begin
+  if line.StartsWith('    ') then
+    exit(false);
+  s := line.Trim;
+  if not s.StartsWith('>') then
+    exit(false);
+  q := TQuoteBlock.Create;
+  blocks.add(q);
+  result := true;
+  // now, instead of grabbing the >, we recurse and get the processor to do that
+  qp := TQuoteProcessor.Create(processor, q);
+  try
+    parse(q, qp);
+  finally
+    qp.Free;
+  end;
+end;
+
 function TCommonMarkParser.parseSeTextHeader(blocks : TObjectList<TBlock>; line: String): boolean;
 var
   p : TParagraphBlock;
@@ -432,7 +610,7 @@ begin
     exit(false);
   if blocks.Count = 0 then
     exit(false);
-  if not (blocks.Last is TParagraphBlock) then
+  if not inPara(blocks, false) then
     exit(false);
   p := blocks.Last as TParagraphBlock;
   if p.closed then
@@ -471,7 +649,7 @@ begin
     exit(false);
   if isWhitespace(line) then
     exit(false);
-  if inPara(blocks) then
+  if inPara(blocks, true) then
     exit(false);
   result := true;
   c := TCodeBlock.Create;
@@ -499,7 +677,68 @@ begin
     c.blocks.Delete(c.blocks.Count - 1);
 end;
 
-procedure TCommonMarkParser.parse(block: TContainerBlock);
+function TCommonMarkParser.parseFencedCodeBlock(blocks: TObjectList<TBlock>; line: String): boolean;
+var
+  toEnd : String;
+  function isEnded: boolean;
+  var
+    s : String;
+  begin
+    s := peekLine;
+    if s.StartsWith('    ') then
+      result := false
+    else
+    begin
+      s := s.Trim;
+      result := allCharsSame(s) and s.StartsWith(toEnd);
+    end;
+  end;
+var
+  c : TCodeBlock;
+  s : String;
+  indent, i : integer;
+begin
+  if line.StartsWith('    ') then
+    exit(false);
+  line := line.Trim;
+  if not (line.StartsWith('`') or line.StartsWith('~')) then
+    exit(false);
+  s := copyWhile(line, [line[1]]);
+  if not (s.StartsWith('```') or s.StartsWith('~~~')) or line.Substring(s.Length).Contains('~') then
+    exit(false);
+  toEnd := s;
+  result := true;
+  c := TCodeBlock.Create;
+  blocks.Add(c);
+  c.fenced := true;
+  if line.Length > s.Length then
+  begin
+    s := line.Substring(s.Length).trim;
+    c.lang := copyTo(s, [' ']);
+  end;
+  s := grabLine;
+  indent := 0;
+  while s[indent+1] = ' ' do
+    inc(indent);
+
+  while not done and not isEnded do
+  begin
+    s := grabLine;
+    if indent > 0 then
+    begin
+      i := 0;
+      while (i < s.Length) and (i < indent) and (s[i+1] = ' ') do
+        inc(i);
+      if i > 0 then
+        s := s.Substring(i);
+    end;
+    c.FBlocks.Add(TTextBlock.Create(escapehtml(s)));
+  end;
+  if not done then
+    grabLine;
+end;
+
+procedure TCommonMarkParser.parse(block: TContainerBlock; processor : TBlockProcessor);
 var
   line : String;
   p : TParagraphBlock;
@@ -509,24 +748,27 @@ begin
   while not done do // must be at start of line here
   begin
     line := peekline;
+    if processor.processLine(line, true) then
+      exit; // ok, we're done with this set of blocks
+
     if parseSeTextHeader(block.blocks, line) then
     else if parseThematicBreak(block.blocks, line) then
     else if parseHeader(block.blocks, line) then
     else if parseCodeBlock(block.blocks, line) then
+    else if parseFencedCodeBlock(block.blocks, line) then
+    else if parseQuoteBlock(block.blocks, line, processor) then
     else
     begin
-      if inPara(block.blocks) then
+      if inPara(block.blocks, true) then
         p := block.Blocks.Last as TParagraphBlock
       else
         p := nil;
-      if line = '' then
+      if (line = '') or isWhitespace(line) then
       begin
         if (p <> nil) then
           p.closed := true;
         grabLine;
       end
-      else if (p = nil) and isWhitespace(line) then
-        grabLine
       else
       begin
         if (p = nil) then
@@ -649,7 +891,10 @@ procedure TCodeBlock.render(b: TStringBuilder);
 var
   c : TBlock;
 begin
-  b.Append('<pre><code>');
+  if lang <> '' then
+    b.Append('<pre><code class="language-'+lang+'">')
+  else
+    b.Append('<pre><code>');
   for c in FBlocks do
   begin
     c.render(b);
@@ -657,6 +902,82 @@ begin
   end;
   b.Append('</code></pre>');
   b.Append(#10);
+end;
+
+{ TQuoteProcessor }
+
+constructor TQuoteProcessor.Create(processor : TBlockProcessor; q: TQuoteBlock);
+begin
+  inherited Create(processor);
+  quote := q;
+end;
+
+function TQuoteProcessor.processLine(var line: string; root : boolean): boolean;
+var
+  l : integer;
+begin
+  result := FParent.processLine(line, false);
+  if not result then
+  begin
+    if startsWithWS(line, '>', l) then
+    begin
+      line := line.subString(l);
+      parser.grab(l);
+    end
+    else if root then // only can do that else we walk into descendents
+    begin
+      // decide whether the block is a continuation, or the end.
+      // it's the end if it's empty, or it starts a new kind of block
+      // plain text isn't the end of the block (lazy :-( )
+      if parser.isWhitespace(line) then
+        result := true
+      else if parser.isBlock(line) then
+        result := true
+    end;
+  end
+end;
+
+{ TBlockProcessor }
+
+constructor TBlockProcessor.Create(processor: TBlockProcessor);
+begin
+  inherited Create;
+  FParent := processor;
+end;
+
+function TBlockProcessor.parser: TCommonMarkParser;
+begin
+  result := FParent.parser;
+end;
+
+{ TDocumentProcessor }
+
+constructor TDocumentProcessor.Create(parser : TCommonMarkParser);
+begin
+  inherited Create(nil);
+  FParser := parser;
+end;
+
+function TDocumentProcessor.parser: TCommonMarkParser;
+begin
+  result := FParser;
+end;
+
+function TDocumentProcessor.processLine(var line: string; root : boolean): boolean;
+begin
+  result := false; // document is only ended by end of source
+end;
+
+{ TQuoteBlock }
+
+procedure TQuoteBlock.render(b: TStringBuilder);
+var
+  c : TBlock;
+begin
+  b.Append('<blockquote>'#10);
+  for c in FBlocks do
+    c.render(b);
+  b.Append('</blockquote>'#10);
 end;
 
 end.
