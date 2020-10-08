49b48348d07f5d63c063c5f9796d3abc69070516
Grahame Grieve <grahameg@gmail.com>
2020-10-09 07:21:52 +1100

    Fix for issue #12

--- source/MarkdownDaringFireball.pas
+++ source/MarkdownDaringFireball.pas
@@ -2994,7 +2994,7 @@ begin
       line := self;
       while (line <> nil) do
       begin
-        while (position < Length(line.value)) and (line.value[1 + position] <> '<') do
+        while (position < Length(line.value)) and ((line.value[1 + position] <> '<') or (position = Length(line.value)-1)) do
           inc(FPosition);
         if (position >= Length(line.value)) then
         begin
